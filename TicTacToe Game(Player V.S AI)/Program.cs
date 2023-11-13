89fe07aba545d03669801a126009bbf16fc7c9e3
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2023-11-13 12:08:32 +0200

    Initialization of Board with a warm welcome to a game of Tic-Tac-Toe!

--- TicTacToe Game(Player V.S AI)/Program.cs
+++ TicTacToe Game(Player V.S AI)/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+class TicTacToe
+{
+    static char[,] board =  { 
+        { '_', '_', '_' }, 
+        { '_', '_', '_' }, 
+        { '_', '_', '_' } };
+
+    static char playerSymbol = 'X';
+    static char cpuSymbol = 'O';
+
+    static void Main()
+    {
+        Console.WriteLine("Welcome to a game of Tic-Tac-Toe!");
+    }
+};
