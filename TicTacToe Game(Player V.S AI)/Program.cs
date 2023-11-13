886788117e83b797b62ab7837037b8c07778e53f
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2023-11-13 12:56:10 +0200

    CPU move declared  and fixed player move

--- TicTacToe Game(Player V.S AI)/Program.cs
+++ TicTacToe Game(Player V.S AI)/Program.cs
@@ -13,7 +13,7 @@ class TicTacToe
 
     static void Main()
     {
-        Console.WriteLine("Welcome to a game of Tic-Tac-Toe!");
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
         PrintBoard();
 
         while (true)
@@ -23,7 +23,7 @@ class TicTacToe
 
             if (CheckWinner(playerSymbol))
             {
-                Console.WriteLine("Player Wins!");
+                Console.WriteLine("Player wins!");
                 break;
             }
 
@@ -39,18 +39,15 @@ class TicTacToe
 
             if (CheckWinner(cpuSymbol))
             {
-                Console.WriteLine("CPU's Wins!:");
+                Console.WriteLine("CPU wins!");
                 break;
             }
 
+            if (IsBoardFull())
             {
-                if (IsBoardFull())
-                {
-                    Console.WriteLine("It's a Tie");
-                    break;
-                }
+                Console.WriteLine("It's a tie!");
+                break;
             }
-
         }
     }
 
@@ -60,34 +57,35 @@ class TicTacToe
         {
             for (int j = 0; j < 3; j++)
             {
-                Console.WriteLine(board[i, j] + "");
+                Console.Write(board[i, j] + " ");
             }
             Console.WriteLine();
         }
         Console.WriteLine();
     }
+
     static bool CheckWinner(char symbol)
     {
-        for(int i = 0; i <3; i++) 
+        for (int i = 0; i < 3; i++)
         {
-            if (board[i,0] == symbol && board[i,1] == symbol && board[i ,2 ] == symbol)
+            if (board[i, 0] == symbol && board[i, 1] == symbol && board[i, 2] == symbol)
                 return true;
-            if (board[0,1] == symbol && board [1,i] == symbol && board[2 ,i] == symbol)
+            if (board[0, i] == symbol && board[1, i] == symbol && board[2, i] == symbol)
                 return true;
         }
-
-        if (board[0,0] == symbol && board[1,1] == symbol && board[2,2] == symbol)
+        if (board[0, 0] == symbol && board[1, 1] == symbol && board[2, 2] == symbol)
             return true;
-        if (board[0,2] == symbol && board[1 ,1] == symbol && board[2 , 0] == symbol)
+        if (board[0, 2] == symbol && board[1, 1] == symbol && board[2, 0] == symbol)
             return true;
 
         return false;
     }
-       static bool IsBoardFull()
+
+    static bool IsBoardFull()
     {
-        for(int i = 0;i < 3;i++)
+        for (int i = 0; i < 3; i++)
         {
-            for(int j = 0;j < 3;j++)
+            for (int j = 0; j < 3; j++)
             {
                 if (board[i, j] == '_')
                     return false;
@@ -95,43 +93,52 @@ class TicTacToe
         }
         return true;
     }
-       static void PleyerMove()
+
+    static void PlayerMove()
     {
         int position;
-        while (true) 
+        while (true)
         {
-            Console.WriteLine("Player's turn - Enter position( 1 -9):");
-            if(int.TryParse(Console.ReadLine(), out position) && position >=1 && position <= 9) 
+            Console.WriteLine("Player's turn - Enter position (1-9): ");
+            if (int.TryParse(Console.ReadLine(), out position) && position >= 1 && position <= 9)
             {
-                int row = (position - 1) / 3;
-                int col = (position - 1) / 3;
+                int index = position - 1;
+                int row = index / 3;
+                int col = index % 3;
 
                 if (board[row, col] == '_')
                 {
-                    if (board[row , col] == '_')
-                    {
-                        board[row, col] = playerSymbol;
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid move. The position is already taken. Try again");
-                    }
+                    board[row, col] = playerSymbol;
+                    break;
                 }
                 else
                 {
-                    Console.WriteLine("Invalid input. Please enter a number between 1 and 9.");
+                    Console.WriteLine("Invalid move. The position is already taken. Try again.");
                 }
             }
-        
-            static void CPUMove()
+            else
             {
-
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 9.");
             }
-        {
-            
         }
     }
+
+    static void CPUMove()
+    {
+        int position;
+        while (true)
+        {
+            Console.WriteLine("CPU's turn - Enter position (1-9): ");
+            position = new Random().Next(1, 10);
+            int index = position - 1;
+            int row = index / 3;
+            int col = index % 3;
+
+            if (board[row, col] == '_')
+            {
+                board[row, col] = cpuSymbol;
+                break;
+            }
+        }
     }
 }
-
